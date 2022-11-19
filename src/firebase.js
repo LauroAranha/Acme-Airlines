d733a6bc04abfba951f6f806af908d3ec8b484cc
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2022-11-19 00:27:01 -0300

    Update nos imports utilizados pelo firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDoc, getDocs } from 'firebase/firestore';
 
+import { getAuth } from 'firebase/auth'
+import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
 //adicione aqui
@@ -9,4 +11,6 @@ const firebaseConfig = {
 //inicializamos o firebase com as configurações 
 const app = initializeApp(firebaseConfig);
 
+export const storage = getStorage()
+export const auth = getAuth()
 export const db = getFirestore();
