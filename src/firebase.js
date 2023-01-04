ecbcdfefb61ee48bdfa323de6c94410fba0a0383
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-01-03 23:16:22 -0300

    Remove unused api key

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDoc, getDocs } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth'
 import { getStorage } from "firebase/storage";
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC0z5g2bVWzyzvwdBDQb7V4Iy1rB2R90-o",
-    authDomain: "acme-airlines.firebaseapp.com",
-    projectId: "acme-airlines",
-    storageBucket: "acme-airlines.appspot.com",
-    messagingSenderId: "583942664111",
-    appId: "1:583942664111:web:56bcd4cb940083ef26c95a",
-    measurementId: "G-3FH2PX88ZF"
+    //insert firebase key
 };
 
 //inicializamos o firebase com as configurações 
